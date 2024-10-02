4d59ee362a10478234e66d99f2597ed8f3845df9
Ameer Muavia Shah <mavee.shah@hotmail.com>
2024-10-02 16:10:43 +0500

    feat: common party ledger report

---
+++ common_party_ledger/common_party_ledger/report/common_party_ledger/common_party_ledger.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, Ameer Muavia Shah and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Common Party Ledger"] = {
+	"filters": [
+
+	]
+};
