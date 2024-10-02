e1ef19203d0701cc8020eb7ddea4003dc524cfbd
Ameer Muavia Shah <mavee.shah@hotmail.com>
2024-10-02 16:08:08 +0500

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Common Party Ledger
+
+Frappe App for Integrated or Common Party Ledger
+
+#### License
+
+MIT
---
+++ common_party_ledger/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Common Party Ledger",
+			"type": "module",
+			"label": _("Common Party Ledger")
+		}
+	]
